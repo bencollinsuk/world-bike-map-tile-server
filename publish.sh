8d8714eb0dde5668af664649dbda3b15fbb804d6
Ben Collins <ben@bencollins.co.uk>
2024-10-26 16:35:28 +0100

    feat: publish script

---
+++ publish.sh
@@ -0,0 +1,13 @@
+# gcloud auth login
+# gcloud config set project simple-cycle-map-365616
+gcloud auth configure-docker europe-central2-docker.pkg.dev
+gcloud container clusters get-credentials wbm-k8s --zone europe-central2
+
+
+docker tag world-bike-map-tile-server-db:latest europe-central2-docker.pkg.dev/simple-cycle-map-365616/wbm-osm/world-bike-map-tile-server-db:latest
+
+docker tag world-bike-map-tile-server:latest europe-central2-docker.pkg.dev/simple-cycle-map-365616/wbm-osm/world-bike-map-tile-server:latest
+
+docker push europe-central2-docker.pkg.dev/simple-cycle-map-365616/wbm-osm/world-bike-map-tile-server-db:latest
+
+docker push europe-central2-docker.pkg.dev/simple-cycle-map-365616/wbm-osm/world-bike-map-tile-server:latest
--- up.sh
+++ up.sh
@@ -2,5 +2,5 @@
 eval $(minikube -p minikube docker-env)
 docker volume create osm-data
 docker volume create osm-tiles
-docker compose up -d
+docker compose up --build
 echo 'http://localhost:8080/?lat=0&lng=0&zoom=2'
